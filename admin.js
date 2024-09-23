26beed0e92a594f2f6271d9d9b688332c3b5fd2f
Utku Çolak <utku_colak_1005@hotmail.com>
2024-09-23 15:44:41 +0300

    added put delete category

--- admin.js
+++ admin.js
@@ -1,14 +1,15 @@
 const BASE_PATH = "http://localhost:8080/"
 const BASE_IMAGE_PATH = "/Users/utii/Documents/GitHub/ecommerce/"
-
 const jwtToken = localStorage.getItem('jwtToken');
+
+var currentId = 0;
 
 async function addProduct() {
     const fileInput = document.getElementById('productImage');
     const productName = document.getElementById('productName').value;
     const productPrice = document.getElementById('productPrice').value;
     const productUnitsInStock = document.getElementById('productUnitsInStock').value;
-    const productCategoryId = document.getElementById('productCategoryId').value; 
+    const productCategoryId = document.getElementById('productCategoryId').value;
     const productActive = document.getElementById('productActive').checked;
 
     const formData = new FormData();
@@ -35,7 +36,9 @@ async function addProduct() {
         if (!response.ok) {
             throw new Error("Product ekleme isteği başarısız durum kodu : " + response.status)
         }
-        return response.json()
+        hideModal('addProductModal');
+        clearModalValues();
+        getAllProduct();
     }).then(data => {
         console.log(data)
     }).catch(error => {
@@ -86,10 +89,30 @@ async function renderProductTable(productList) {
     });
 }
 
-function deleteProduct(productId) {
-    const confirmed = confirm("Are you sure you want to delete this product?");
-    if (confirmed) {
-        fetch(BASE_PATH + "product/" + productId, {
+function showDeleteProductModal(productId) {
+    currentId = productId;
+    const deleteProductModal = bootstrap.Modal.getOrCreateInstance(document.getElementById('deleteProductModal'))
+    deleteProductModal.show();
+}
+
+function hideModal(modalId) {
+    const deleteProductModal = bootstrap.Modal.getOrCreateInstance(document.getElementById(modalId))
+    deleteProductModal.hide();
+}
+
+
+function clearModalValues() { 
+    document.getElementById('productImage').value = '';
+    document.getElementById('productName').value = '';
+    document.getElementById('productPrice').value = '';
+    document.getElementById('productUnitsInStock').value = '';
+    document.getElementById('productCategoryId').value = '';
+    document.getElementById('productActive').checked = '';
+}
+
+function deleteProduct() {    
+    if (currentId !== 0) {
+        fetch(BASE_PATH + "product/" + currentId, {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
@@ -99,6 +122,7 @@ function deleteProduct(productId) {
             if (!response.ok) {
                 throw new Error("Product silme isteği başarısız durum kodu : " + response.status)
             }
+            hideModal('deleteProductModal');
             getAllProduct();
         }).catch(error => {
             console.error('Error:', error);
@@ -114,7 +138,7 @@ function updateProduct(productId) {
             'Authorization': 'Bearer ' + jwtToken
         }
     }).then(response => {
-        if (!response.ok) { 
+        if (!response.ok) {
             throw new Error("Product getirme isteği başarısız durum kodu : " + response.status)
         }
         return response.json();
@@ -151,7 +175,7 @@ function saveUpdatedProduct() {
     };
 
     const formData = new FormData();
-    formData.append('file', feditedSelectedImage = updateProductImage.files[0]); 
+    formData.append('file', feditedSelectedImage = updateProductImage.files[0]);
     formData.append('product', new Blob([JSON.stringify(productData)], { type: 'application/json' }));
 
     fetch(BASE_PATH + "product/update", {
@@ -170,8 +194,6 @@ function saveUpdatedProduct() {
         console.error('Error:', error);
     });
 
-    //update modal'ı kapatmamız gerekiyor.
-    // $('#updateProductModal').modal('hide');
 }
 
 async function closeUpdateProductModal() {
--- category.js
+++ category.js
@@ -1,6 +1,7 @@
 const jwtToken = localStorage.getItem('jwtToken');
 const BASE_PATH = "http://localhost:8080/"
 
+var currentCategoryId = 0;
 function getAllCategory() {
     fetch(BASE_PATH + "category", {
         method: 'GET',
@@ -29,12 +30,141 @@ function displayCategories(categories) {
             <td>${category.id}</td>
             <td>${category.name}</td>
             <td>
-                <button class="btn btn-warning" onclick="updateCategory(${category.id})">Update</button>
-                <button class="btn btn-danger" onclick="deleteCategory(${category.id})">Delete</button>
+                <button class="btn btn-warning" onclick="getCategoryAndShowModal (${category.id})">Update</button>
+                <button class="btn btn-danger" onclick="showDeleteCategoryModal(${category.id})">Delete</button>
             </td>
         `;
     });     
 }
+
+function getCategoryAndShowModal(categoryId) {
+    fetch(BASE_PATH + "category/" + categoryId, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + jwtToken
+        }
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error("Category getirme isteği başarısız durum kodu : " + response.status)
+        }
+        return response.json();
+    }).then(category => {
+        document.getElementById('updateCategoryId').value = category.id;
+        document.getElementById('updateCategoryName').value = category.name;
+        const updateCategoryModal = bootstrap.Modal.getOrCreateInstance(document.getElementById('updateCategoryModal'));
+        updateCategoryModal.show();
+    }).catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+
+function updateCategory() {
+    const categoryId = document.getElementById('updateCategoryId').value
+    const categoryName = document.getElementById('updateCategoryName').value
+
+    bodyData =JSON.stringify({ 
+        id: categoryId,
+        name: categoryName
+    })
+
+    fetch(BASE_PATH + "category/update", {
+        method: 'PUT',
+        body: bodyData,  
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + jwtToken
+        }
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error("Category PUT isteği başarısız durum kodu : " + response.status)
+        }
+        return response.json();
+    }).then(category => {
+        hideModal('updateCategoryModal');
+        showSuccessAlert("Category updated successfully!");
+        getAllCategory();
+    }).catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+
+function hideModal(modalId) {
+    const deleteProductModal = bootstrap.Modal.getOrCreateInstance(document.getElementById(modalId))
+    deleteProductModal.hide();
+}
+
+
+function showDeleteCategoryModal(categoryId) {
+    currentCategoryId = categoryId;
+    const deleteProductModal = bootstrap.Modal.getOrCreateInstance(document.getElementById('deleteCategoryModal'))
+    deleteProductModal.show();
+}
+
+function deleteCategory() {    
+    if (currentCategoryId !== 0) {
+        fetch(BASE_PATH + "category/" + currentCategoryId, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + jwtToken
+            }
+        }).then(async response => {
+            if (!response.ok) {
+                const data = await response.json();
+                showFailAlert(data.message);
+                throw new Error(data.message);
+            }
+            //hideModal('deleteProductModal');
+            getAllProduct();
+        }).catch(error => {
+            hideModal('deleteProductModal'); //TODO does not work..
+        });
+    }
+}
+function showSuccessAlert (message) {
+    let alert = document.getElementById('success-alert');
+    alert.style.display = 'block';
+    alert.style.opacity = 1;
+    
+    let alertMessage = document.getElementById('successAlertMessage');
+    alertMessage.textContent = message;
+    setTimeout(() => {
+        let opacity = 1;
+        let timer = setInterval(() => {
+            if (opacity <= 0.1) {
+                clearInterval(timer);
+                alert.style.display = 'none';
+            } 
+            alert.style.opacity = opacity;
+            opacity -= opacity * 0.1;
+        }, 50)
+    }, 3000)
+}
+        
+
+function showFailAlert (message) {
+    let alert = document.getElementById('fail-alert');
+    alert.style.display = 'block';
+    alert.style.opacity = 1;
+    
+    let alertMessage = document.getElementById('failAlertMessage');
+    alertMessage.textContent = message;
+    setTimeout(() => {
+        let opacity = 1;
+        let timer = setInterval(() => {
+            if (opacity <= 0.1) {
+                clearInterval(timer);
+                alert.style.display = 'none';
+            } 
+            alert.style.opacity = opacity;
+            opacity -= opacity * 0.1;
+        }, 50)
+    }, 3000)
+}
+    
 
 
 document.addEventListener("DOMContentLoaded", async () => {
@@ -51,7 +181,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                 'Content-Type': 'application/json',
                 'Authorization': 'Bearer ' + jwtToken
             },
-            body: JSON.stringify({
+            body: JSON.stringify({ 
                 name: categoryName
             })
         }).then(response => {
@@ -65,5 +195,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                 console.error('Error:', error);
             })
         })
+
+        
 
 });
